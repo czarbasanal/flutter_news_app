2181a3a261fd4d0b76fbee143a382fa6de9a61b9
Czar Basanal <czarbasanal@gmail.com>
2023-10-24 21:12:19 +0800

    initialize news app repo

---
+++ lib/app_styles.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color white = Color(0xffffffff);
+const Color lightWhite = Color(0xffeff5f4);
+const Color lighterWhite = Color(0xfffcfcfc);
+
+const Color grey = Color(0xff9397a0);
+const Color lightGrey = Color(0xffa7a7a7);
+
+const Color blue = Color(0xff5474fd);
+const Color lightBlue = Color(0xff83b1ff);
+const Color lighterBlue = Color(0xffc1d4f9);
+
+const Color darkBlue = Color(0xff19202d);
+
+const double borderRadius = 16.0;
+
+final border = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(borderRadius),
+  borderSide: BorderSide.none,
+);
+
+final poppinsBold = GoogleFonts.poppins(
+  color: darkBlue,
+  fontWeight: FontWeight.w700,
+);
+
+final poppinsSemibold = GoogleFonts.poppins(
+  color: darkBlue,
+  fontWeight: FontWeight.w600,
+);
+
+final poppinsMedium = GoogleFonts.poppins(
+  color: darkBlue,
+  fontWeight: FontWeight.w500,
+);
+
+final poppinsRegular = GoogleFonts.poppins(
+  color: darkBlue,
+  fontWeight: FontWeight.w400,
+);
---
+++ lib/home_screen_ref.dart
@@ -0,0 +1,371 @@
+import 'package:flutter/material.dart';
+import 'app_styles.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+        ),
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 51,
+                width: 51,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(borderRadius),
+                  color: lightBlue,
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                      'https://cdn3d.iconscout.com/3d/premium/thumb/man-avatar-6299539-5187871.png',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome Back!',
+                    style: poppinsBold.copyWith(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  Text(
+                    'Monday, 31 October',
+                    style: poppinsRegular.copyWith(
+                      color: grey,
+                      fontSize: 12.0,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+              color: white,
+              boxShadow: [
+                BoxShadow(
+                  color: darkBlue.withOpacity(0.051),
+                  offset: const Offset(0.0, 3.0),
+                  blurRadius: 24.0,
+                  spreadRadius: 0.0,
+                )
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    style: poppinsRegular.copyWith(
+                      color: blue,
+                      fontSize: 14.0,
+                    ),
+                    controller: TextEditingController(),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 13.0,
+                      ),
+                      hintText: 'Search for article',
+                      border: border,
+                      errorBorder: border,
+                      disabledBorder: border,
+                      focusedBorder: border,
+                      focusedErrorBorder: border,
+                      hintStyle: poppinsRegular.copyWith(
+                        color: lightGrey,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: blue,
+                    borderRadius: BorderRadius.circular(borderRadius),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      'assets/search_icon.svg',
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            height: 14,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(
+                    right: 38,
+                  ),
+                  child: Text(
+                    '#Health',
+                    style: poppinsMedium.copyWith(
+                      color: grey,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 304,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(
+                  decelerationRate: ScrollDecelerationRate.normal),
+              shrinkWrap: true,
+              itemCount: 5,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.all(12),
+                  margin: const EdgeInsets.only(
+                    right: 20,
+                  ),
+                  height: 304,
+                  width: 255,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(borderRadius),
+                    color: white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: darkBlue.withOpacity(0.051),
+                        offset: const Offset(0.0, 3.0),
+                        blurRadius: 24.0,
+                        spreadRadius: 0.0,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 164,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(borderRadius),
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              'https://a.cdn-hotels.com/gdcs/production143/d1112/c4fedab1-4041-4db5-9245-97439472cf2c.jpg',
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      Flexible(
+                        child: Text(
+                          'Bali - Uniqure, unmatched. There is no other place like Bali in this world.',
+                          style: poppinsBold.copyWith(
+                            fontSize: 18.0,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 19,
+                                backgroundColor: lightBlue,
+                                backgroundImage: NetworkImage(
+                                  'https://blogger.googleusercontent.com/img/a/AVvXsEiRB_dB-wXqJdvt26dkR-vqOXUjacfxAQIgFNMHl_czjMNDOh6VZVc-muCczDKZh-VU0JqUYV1M9h25ZooLGqhVfwexQO6zNY1jxeMDu0-SpfEPe8xkF7re1eldAkKld9Ct1YzesFmHpQK9wlPK330AXA85gsmDBURTQm3i7r08g6vO7KNtAPyDgeUIaQ=s740',
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Czar Basanal',
+                                    style: poppinsSemibold.copyWith(
+                                      fontSize: 14.0,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Sep 29, 2022',
+                                    style: poppinsRegular.copyWith(
+                                      color: grey,
+                                      fontSize: 14.0,
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                          Container(
+                            height: 38,
+                            width: 38,
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(borderRadius),
+                              color: lightWhite,
+                            ),
+                            child: SvgPicture.asset(
+                              'assets/share_icon.svg',
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Short for You',
+                style: poppinsBold.copyWith(
+                  fontSize: 20.0,
+                ),
+              ),
+              Text(
+                'View all',
+                style: poppinsMedium.copyWith(
+                  color: blue,
+                  fontSize: 14.0,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 19,
+          ),
+          SizedBox(
+            height: 88,
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.all(9),
+                  margin: const EdgeInsets.only(
+                    right: 20,
+                  ),
+                  width: 208,
+                  height: 88,
+                  decoration: BoxDecoration(
+                    color: white,
+                    borderRadius: BorderRadius.circular(borderRadius),
+                    boxShadow: [
+                      BoxShadow(
+                        color: darkBlue.withOpacity(0.051),
+                        offset: const Offset(0.0, 3.0),
+                        blurRadius: 24.0,
+                        spreadRadius: 0.0,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 70,
+                        padding: const EdgeInsets.all(26),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(borderRadius),
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              'https://homeiswhereyourbagis.com/wp-content/uploads/2019/06/lombok-mustsees-mawun-beach-drohne.jpg',
+                            ),
+                          ),
+                        ),
+                        child: SvgPicture.asset(
+                          'assets/play_icon.svg',
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      Flexible(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Top Trending Island in 2022',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: poppinsSemibold.copyWith(
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 14.0,
+                            ),
+                            Row(
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/eye_icon.svg',
+                                ),
+                                const SizedBox(
+                                  width: 4,
+                                ),
+                                Text(
+                                  '40,999',
+                                  style: poppinsMedium.copyWith(
+                                    color: grey,
+                                    fontSize: 14.0,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
